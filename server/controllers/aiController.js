160320cc6fd1c05b2f7ae389dae7314ec4765527
Ayush Kumar Singh <iamayushsingh02@gmail.com>
2025-12-27 16:23:52 +0530

    500 error

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -45,42 +45,6 @@ export const generateArticle = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 }
-// export const generateBlogTitle = async (req, res) => {
-//   try {
-//     const { userId } = req.auth();
-//     const { prompt } = req.body;
-//     const plan = req.plan;
-//     const free_usage = req.free_usage;
-
-//     if (plan !== 'premium' && free_usage >= 100) {
-//       return res.json({ success: false, message: "Limit reached. Upgrade to continue." });
-//     }
-//     const response = await AI.chat.completions.create({
-//       model: "gemini-1.5-flash",
-//       messages: [{ role: "user", content: prompt, }],
-//       temperature: 0.7,
-//       max_tokens: 100
-//     });
-
-//     const content = response.choices[0].message.content
-
-//     await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
-
-//     if (plan !== 'premium') {
-//       await clerkClient.users.updateUserMetadata(userId, {
-//         privateMetadata: {
-//           free_usage: free_usage + 1
-//         }
-//       });
-//     }
-
-//     res.json({ success: true, content });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.json({ success: false, message: error.message });
-//   }
-// }
-
 export const generateBlogTitle = async (req, res) => {
   try {
     const { userId } = req.auth();
@@ -109,14 +73,15 @@ export const generateBlogTitle = async (req, res) => {
         }
       });
     }
+    // Set response status to 404 and send a message
+    res.status(500).send('Sorry, we cannot find that!');
 
-    res.json({ success: true, content });
+    // res.json({ success: true, content });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
   }
 }
-
 export const generateImage = async (req, res) => {
   try {
     const { userId } = req.auth();
